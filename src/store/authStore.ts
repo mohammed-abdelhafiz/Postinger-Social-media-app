748934ce60ed55cd5a8ae973aee3368275728812
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-03-08 02:46:18 +0200

    working on auth

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,11 +1,16 @@
-// import { create } from "zustand"
+import { User } from "@/features/auth/types/auth.types";
+import { create } from "zustand";
 
-// interface AuthStore {
-//   user: any
-//   setUser: (user:any)=>void
-// }
+interface AuthStore {
+  user: User | null;
+  accessToken: string | null;
+  authenticate: (user: User, token: string) => void;
+  clearAuthState: () => void;
+}
 
-// export const useAuthStore = create<AuthStore>((set)=>({
-//   user:null,
-//   setUser:(user)=>set({user})
-// }))
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
+  accessToken: null,
+  authenticate: (user, token) => set({ user, accessToken: token }),
+  clearAuthState: () => set({ user: null, accessToken: null }),
+}));
